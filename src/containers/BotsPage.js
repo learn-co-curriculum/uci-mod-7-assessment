a9ec657600d60e24fd21089815f663751b7ab25d
maxwellbenton <maxwellbenton@gmail.com>
2019-06-19 16:09:01 -0400

    sets starter components/containers

---
+++ src/containers/BotsPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class BotsPage extends React.Component {
+
+  //start here with your code for step one - Fetch Bots
+
+  render() {
+    return (
+      <div>
+        {/* put your components here */}
+      </div>
+    );
+  }
+
+}
+
+export default BotsPage;
